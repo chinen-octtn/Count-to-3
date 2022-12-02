2a046f71d9c288c7651a39f13914813fc29b7939
maashi <octtn8c@gmail.com>
2022-12-03 00:59:18 +0900

    add: オフライン対応

--- sw.js
+++ sw.js
@@ -2,3 +2,24 @@ self.addEventListener("fetch", function (e) {
   // ここは空でもOK
   console.log("Service Worker");
 });
+
+// Workboxライブラリのインポート
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: "/index.html",
+    revision: "123",
+  },
+  {
+    url: "/asetts/css/style.css",
+    revision: "123",
+  },
+  {
+    url: "/asetts/js/script.js",
+    revision: "123",
+  },
+]);
